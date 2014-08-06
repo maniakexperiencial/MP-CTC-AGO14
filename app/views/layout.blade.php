a22fce860ff488377c0fd3f2f989c78e714047e9
alvaro <alvaro@maniak.com.mx>
2014-08-06 04:18:07 -0700

    -Maquetado Principio

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,16 +3,17 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Laravel PHP Framework</title>
 
     {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', array('media' => 'screen')) }}
-    {{ HTML::style('css/style.css') }}
+
 
 
     {{ HTML::style('css/semantic.min.css') }}
     {{ HTML::style('css/semantic.css') }}
 
-
+    {{ HTML::style('css/style.css') }}
 
     {{ HTML::script('js/semantic.js', array('media' => 'screen')) }}
     {{ HTML::script('js/semantic.min.js', array('media' => 'screen')) }}
@@ -66,7 +67,7 @@
                             <a href="{{ URL::route('kids') }}">Niños</a>
                         </li>
                         <li>
-                            <a href="{{ URL::route('kids') }}">Papás</a>
+                            <a href="{{ URL::route('papas') }}">Papás</a>
                         </li>
                         <li>
                             <a href="{{ URL::route('doctores') }}">Médicos</a>
@@ -76,7 +77,8 @@
                         </li>
                     </ul>
                 </nav>
-             </div>
+            </div>
+
 
         </div>
 
@@ -91,7 +93,7 @@
                     <option value="mercedes">Mercedes</option>
                     <option value="audi">Audi</option>
                 </select>
-             </div>
+            </div>
         </div>
     </div>
 </header>
@@ -99,19 +101,19 @@
 <div class="ui one column page grid">
     <div class="column" id="content_wrap">
         <div class="ui grid">
-        <div class="row">
-            <div class="four wide column">
-                <div class="Title_section"></div>
-                @yield('left_menu')
+            <div class="row">
+                <div class="four wide column">
+                    <div class="Title_section"></div>
 
-            </div>
-            <div class="twelve wide column">
-                   <div class="Title_section">@yield('title_section')</div>
+
+                </div>
+                <div class="twelve wide column ">
+                    <div class="Title_section">@yield('title_section')</div>
                     @yield('content_center')
+                </div>
+
+
             </div>
-
-
-        </div>
         </div>
 
     </div>
@@ -120,8 +122,8 @@
     <div class="ui one column page grid">
         <div class="column" >
             contactanos
-         </div>
-     </div>
+        </div>
+    </div>
 </div>
 @yield('javacode')
 </body>
