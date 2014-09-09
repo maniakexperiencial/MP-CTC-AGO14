86b40182ebd65c00b8b7bcb4616d4a733c7b2806
alvaro <alvaro@maniak.com.mx>
2014-09-09 11:40:31 -0700

    -quitar meta en header para mobiles

--- app/views/papas/layout.blade.php
+++ app/views/papas/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0" />-->
     <title>Janssen</title>
 
     {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', array('media' => 'screen')) }}
